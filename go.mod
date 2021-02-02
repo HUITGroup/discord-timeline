cdb685a560cc257370afc9fb85d7a39b5fdb20d5
ayumin <kosyu79@gmail.com>
2021-02-02 15:13:03 +0900

    とりあえずチャンネル名表示する

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/shoumoji/discord-timeline
 
 go 1.15
 
-require github.com/bwmarrin/discordgo v0.23.2-0.20210127175531-5f6bb2d120cc
+require (
+	github.com/bwmarrin/discordgo v0.23.2-0.20210127175531-5f6bb2d120cc
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	const token string = "test"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loding .env file")
+	}
+	token := os.Getenv("TOKEN")
 
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
@@ -18,11 +27,35 @@ func main() {
 	// timesチャンネルで発生したsendMessageを全てtimelineチャンネルに送信
 	dg.AddHandler(sendTimeline)
 
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press Ctrl-C to exit")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
 }
 
-func sendTimeline(s *discordgo.Session, m *discordgo.MessageCreate){
-	reciveChannelID := m.ChannelID
+func sendTimeline(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// bot自身の発言は処理する必要なし
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	discordChannel, err := s.Channel(m.ChannelID)
+	if err != nil {
+		fmt.Println("Error could not get Channel info,", err)
+		return
+	}
+	//
+	fmt.Println(discordChannel.Name)
+
 	// todo チャンネルidからチャンネル名を取得したい
-	reciveChannelName := reciveChannelID.
-	if 
 }
